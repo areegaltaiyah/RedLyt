2c510c501f791fd16aabce9b40316f333bab4130
Areeg <Areegaltaiyah@gmail.com>
2026-02-01 11:25:13 +0300

    Content

---
+++ RedLyt/RedLyt/RedLytApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedLytApp.swift
+//  RedLyt
+//
+//  Created by Areeg Altaiyah on 01/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct RedLytApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
