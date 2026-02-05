9dcbc63e3dd7f28be5509e1dc0e64b467783bc55
Areeg <Areegaltaiyah@gmail.com>
2026-02-05 10:20:16 +0300

    Modification to the FAQ page title

---
+++ RedLyt/RedLyt/Model/PodcastHostModel.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  RedLyt
+//
+//  Created by Areeg Altaiyah on 01/02/2026.
+//
+
+import SwiftUI
+
+struct PodcastHostModel: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
